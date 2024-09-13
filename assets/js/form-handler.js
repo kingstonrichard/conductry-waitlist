bc7aa62a58acdcedc753e87375dfbc05f395b2fb
Richard Kingston <richardkingston@gmail.com>
2024-09-13 22:41:55 +0100

    Remove script tags from within .js file

--- assets/js/form-handler.js
+++ assets/js/form-handler.js
@@ -1,25 +1,25 @@
-<script>
-  document.getElementById('waiting-list-form').addEventListener('submit', async function(event) {
-    event.preventDefault();
+document.getElementById('waiting-list-form').addEventListener('submit', async function (event) {
+  event.preventDefault();
 
   const form = event.target;
   const formData = new FormData(form);
 
   try {
-      const response = await fetch(form.action, {
-    method: form.method,
-  body: formData,
-  headers: {'Accept': 'application/json' }
+    const response = await fetch(form.action,
+      {
+        method: form.method,
+        body: formData,
+        headers: { 'Accept': 'application/json' }
       });
 
-  if (response.ok) {
-    document.getElementById('success-message').style.display = 'block';
-  form.reset();
-      } else {
-        throw new Error('Form submission error');
-      }
-    } catch (error) {
-    document.getElementById('error-message').style.display = 'block';
+    if (response.ok) {
+      document.getElementById('success-message').style.display = 'block';
+      form.reset();
+    } else {
+      throw new Error('Form submission error');
     }
-  });
-</script>
+  }
+  catch (error) {
+    document.getElementById('error-message').style.display = 'block';
+  }
+});
